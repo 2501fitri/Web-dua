3a0dae9eeb763220a9e95e2d4351d3efa5270873
SaimaFitri <sp797615@gmail.com>
2025-02-24 23:15:21 +0700

    praktikum

---
+++ praktikum/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+//mendefenisikan variabel
+$nama = 'saima';
+$umur = 20;
+$berat = 60.68;
+
+echo 'Nama saya' . $nama; 
+echo '<br/>Umur :' . $umur. 'Tahun';
+echo '<br/>Berat :' . $berat;
+?>
